77739a3137e81e077734d4b5dc17586048ac1410
Monica Hackney <mhackney@HAGERTY.com>
2025-11-24 15:28:51 -0500

    tutorial updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ import { lusitana } from "@/app/ui/fonts";
 
 import Image from "next/image";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
